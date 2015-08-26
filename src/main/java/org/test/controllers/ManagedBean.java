aa64c11e45336cc52260a4a0de8b87f024d7f5f0
jbeef <jbeef@Ubuntu>
2015-08-26 09:24:18 +0300

    V 1.1 SimpleUpload portlet works

--- src/main/java/org/test/controllers/ManagedBean.java
+++ src/main/java/org/test/controllers/ManagedBean.java
@@ -1,11 +1,22 @@
 package org.test.controllers;
 
-import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 /**
  *
@@ -18,27 +29,40 @@ public class ManagedBean {
     @Inject
     FacesContext context;
 
-    private String name;
+    private UploadedFile file;
 
     public ManagedBean() {
     }
 
-    @PostConstruct
-    private void init() {
-        name = "Jbeef";
+    public void handleFileUpload(FileUploadEvent event) throws FileNotFoundException, IOException {
+
+        String extension = FilenameUtils.getExtension(event.getFile().getFileName());
+        String path = "/home/jbeef/Desktop";
+        String newName = "akisfile";
+
+        InputStream input = event.getFile().getInputstream();
+        OutputStream output = new FileOutputStream(new File(path, newName));
+
+        try {
+            IOUtils.copy(input, output);
+        } catch (IOException ex) {
+            Logger.getLogger(ManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            IOUtils.closeQuietly(input);
+            IOUtils.closeQuietly(output);
+        }
+
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success!", "The entry saved. "));
+
     }
 
-    public void submit() {
-        System.out.println("##  Name: " + name);
-        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Warning!", "The entry saved. " + name));
+//-------------------------------- Getters/Setters ---------------------------------------    
+    public UploadedFile getFile() {
+        return file;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setFile(UploadedFile file) {
+        this.file = file;
     }
 
 }
